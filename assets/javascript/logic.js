d33356893db79b4f4e59158da836ee88af5fb8a5
EmirVelazquez <emirleovelazquez@gmail.com>
2019-11-14 18:28:30 -0600

    added logic for removing active link state if name is clicked by user

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,17 @@
 //Master function that will only execute after the DOM has loaded
 $(document).ready(function () {
     //Custom Logic for Nav
+    //This block adds and removes active state on links
     $("#navBar .navbar-nav a").on("click", function () {
         $("#navBar .navbar-nav").find("li.active").removeClass("active");
         $(this).parent("li").addClass("active");
     });
+    //This block revomes active state from all links if my name is clicked
+    $("#navName").on("click", function () {
+        $("#navBar .navbar-nav").find("li.active").removeClass("active");
+    });
+
+
+
+
 }); 
